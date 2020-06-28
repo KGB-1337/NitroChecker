a9aecc00a68fdcd2cadeed0687af00ff10cb34d8
Nathan <60408318+NathanPrivate@users.noreply.github.com>
2020-06-28 22:30:36 +0000

    Add files via upload

---
+++ Nathans Nitro Checker/Checker.py
@@ -0,0 +1,37 @@
+from colorama import Fore, Style, init
+import requests
+init()
+
+def checkNitro(code, userToken):
+    
+    URL = f'https://discordapp.com/api/v6/entitlements/gift-codes/{code}/redeem'
+    result = requests.post(URL, headers={'authorization': userToken}).text
+    if 'nitro' in result:
+        return True
+    else: 
+        return False
+
+
+userToken = str(input('Enter Your Discord Token. \n >'))
+
+VALID_CODES_FILE   = "valid_nitro_codes.txt"
+INVALID_CODES_FILE = "invalid_nitro_codes.txt"
+CODES_FILE         = "nitro_codes.txt"
+
+validTokens_file   = open(VALID_CODES_FILE, 'a+')
+invalidTokens_file = open(INVALID_CODES_FILE, 'a+')
+
+with open(CODES_FILE , 'r') as codes_file:
+    content = codes_file.readlines()
+    codes = list(filter(None, list(map(lambda x: str.replace(x, "\n", ""), content)))) # removes shit lol
+
+    for code in codes:
+        status = checkNitro(code, userToken)
+        if status is True:
+            print(f'{Fore.GREEN + Style.BRIGHT} {code}', Fore.WHITE + Style.NORMAL)
+            validTokens_file.write(code + "\n")
+        else:
+            print(f'{Fore.RED + Style.BRIGHT} {code}', Fore.WHITE + Style.NORMAL)
+            invalidTokens_file.write(code + "\n")
+print('Done.')
+input()
